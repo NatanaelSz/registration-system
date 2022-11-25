0a3ea9f9c596bf017f56c4af8064cae58cd7afc2
NatanaelSz <natanaelcell23@gmail.com>
2022-11-25 12:12:05 -0300

    atualizando um console.log

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Fiz uma modificação sem voce ver")
 //mais um console.log de teste
-console.log("teste")
+console.log("Enviando para o git")
