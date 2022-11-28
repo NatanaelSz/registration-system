a4ffd3dd092cd7bb6656eb4b72dc71e81e3c66b2
NatanaelSz <natanaelcell23@gmail.com>
2022-11-28 10:56:11 -0300

    teste de commit

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Fiz uma modificação sem voce ver")
 //mais um console.log de teste
 console.log("teste")
+console.log("mais um teste")
